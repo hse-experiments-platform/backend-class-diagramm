e1ce4d70b1fbe807a4fe27d3d5833e3c7c18cf91
Lev Tulyavko <wot.k@mail.ru>
2024-01-18 14:20:53 +0000

    feat: add deps

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"github.com/hse-experiments-platform/backend-class-diagramm/datasets"
+	_ "github.com/hse-experiments-platform/backend-class-diagramm/datasets"
+	_ "github.com/hse-experiments-platform/backend-class-diagramm/enum"
+	_ "github.com/hse-experiments-platform/backend-class-diagramm/gateway"
+	_ "github.com/hse-experiments-platform/backend-class-diagramm/launcher"
+	_ "github.com/hse-experiments-platform/backend-class-diagramm/models"
 )
 
 func main() {
-	_ = datasets.Dataset{}
 }
